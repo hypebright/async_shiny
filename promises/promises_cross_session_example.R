31917a7bc1de899984db20c0d11fc0baf88e9132
Veerle van Leemput <info@hypebright.nl>
2023-04-02 15:10:45 +0200

    add cross-session example using promises

---
+++ promises/promises_cross_session_example.R
@@ -0,0 +1,71 @@
+
+library(promises)
+library(future)
+
+# Options for asynchronous strategies: multisession,
+# multicore (not Windows/RStudio), cluster
+plan(multisession)
+
+ui <- fluidPage(
+  titlePanel("Using promises in Shiny (cross-session)"),
+  
+  p("Note that clicking 'Start Expensive Job' does not seem
+    like a revolution, but the long computation (our complex Sys.sleep() 😉)
+    is not blocking for the other users in the same session. However,
+    it does block the app for the current user until the promise is resolved.
+    This example demonstrates that by rendering two outputs: a table and text.
+    The table uses a promise, the text does not. Yet, we have to wait for the
+    table to render until the text appears. So in this example, we
+    demonstrate cross-session asynchronicity, not inner-session."),
+  
+  selectInput(
+    inputId = "rows",
+    label = "Number of rows",
+    choices = c(10, 50, 100, 150),
+    selected = 10
+  ),
+  
+  actionButton(inputId = "start_job",
+               label = "Start Expensive Job",
+               icon = icon("bolt")),
+  
+  tableOutput("result_table"),
+  
+  textOutput("text")
+)
+
+server <- function(input, output, session) {
+
+  # Display the table data
+  output$result_table <- renderTable({
+    
+    req(input$start_job)
+    
+    print("Starting job now")
+    
+    # reactive values and reactive expressions cannot be read from
+    # within a future, therefore, you need to read any reactive
+    # values/expressions in advance of launching the future
+    rows <- input$rows
+    
+    future_promise({
+      # long computation
+      Sys.sleep(5)
+      # filter
+      iris %>% head(rows)
+    })
+    
+  })
+  
+  # Display some text to demonstrate inner-session blocking
+  output$text <- renderText({
+    
+    req(input$start_job)
+    
+    "Only when the promise is resolved, this message appears"
+    
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
